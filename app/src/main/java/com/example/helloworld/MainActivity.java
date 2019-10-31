2df189427cc7900139e04ede6a9fa4aec0ae73ea
OKee <mzfadlii@gmail.com>
2019-10-31 21:21:15 +0700

    tampilan signup+signin

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView teks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teks=(TextView)findViewById(R.id.font);
+
+        Typeface cFont=Typeface.createFromAsset(getAssets(),"font/Kirani A Handwritten Font Demo.otf");
+        teks.setTypeface(cFont);
+
+        LinearLayout go =(LinearLayout) findViewById(R.id.go);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
