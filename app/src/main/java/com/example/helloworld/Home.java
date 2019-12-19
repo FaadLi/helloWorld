49389885cef1e52bac45889f1b4d6e846f69ef21
FaadLi <37612569+FaadLi@users.noreply.github.com>
2019-12-19 13:04:57 +0700

    shot camera

--- app/src/main/java/com/example/helloworld/Home.java
+++ app/src/main/java/com/example/helloworld/Home.java
@@ -1,12 +1,18 @@
 package com.example.helloworld;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,6 +21,7 @@ public class Home extends AppCompatActivity {
 
     TextView txtEmail;
     Button btnSignOut;
+    ImageView btnGambar;
 
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
@@ -23,6 +30,9 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btnGambar =findViewById(R.id.btnambilgambar);
+
 
         txtEmail    = findViewById(R.id.txtEmail);
         btnSignOut  = findViewById(R.id.btnSignOut);
@@ -42,5 +52,51 @@ public class Home extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        btnGambar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isDeviceSupportCamera()) {
+                    Toast.makeText(getApplicationContext(),"Camera di device anda tidak tersedia", Toast.LENGTH_LONG).show();
+                }else {
+                    captureImage();
+                }
+            }
+        });
     }
+    private void captureImage() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Intent cameraIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        //cameraIntent.setType("image/*");
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivityForResult(takePictureIntent, 100);
+        }
+    }
+
+    private boolean isDeviceSupportCamera() {
+        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            //this device has a camera
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            Uri returnUri;
+//            returnUri = data.getData();
+            Intent intent = new Intent(this, Home.class);
+            //intent.putExtra("imageuri",returnUri.toString());
+            intent.putExtras(extras);
+            startActivity(intent);
+        }
+    }
+
+
 }
